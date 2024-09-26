7265a6aa78733dac0b15c46bf840b3f010cd93e7
Venkat Sundaram <121580855+venkat-sundaram@users.noreply.github.com>
2024-09-26 07:08:48 -0400

    Update models.go

--- models/models.go
+++ models/models.go
@@ -34,8 +34,8 @@ func AllBooks() ([]Book, error) {
 // The user input is not parameterized. Instead of using fmt.Sprintf() to build
 // the query, you should be using a parameterized query.
 func NameQuery(r string) ([]Book, error) {
-	// Fix: rows, err := DB.Query("SELECT * FROM books WHERE name = ?", r)
-	rows, err := DB.Query(fmt.Sprintf("SELECT * FROM books WHERE name = '%s'", r))
+	rows, err := DB.Query("SELECT * FROM books WHERE name = ?", r)
+	// rows, err := DB.Query(fmt.Sprintf("SELECT * FROM books WHERE name = '%s'", r))
 	if err != nil {
 		return nil, err
 	}
@@ -53,8 +53,8 @@ func NameQuery(r string) ([]Book, error) {
 // The user input is not parameterized. Instead of using fmt.Sprintf() to build
 // the query, you should be using a parameterized query.
 func AuthorQuery(r string) ([]Book, error) {
-	// Fix: rows, err := DB.Query("SELECT * FROM books WHERE author = ?", r)
-	rows, err := DB.Query(fmt.Sprintf("SELECT * FROM books WHERE author = '%s'", r))
+	rows, err := DB.Query("SELECT * FROM books WHERE author = ?", r)
+	// rows, err := DB.Query(fmt.Sprintf("SELECT * FROM books WHERE author = '%s'", r))
 	if err != nil {
 		return nil, err
 	}
@@ -72,8 +72,8 @@ func AuthorQuery(r string) ([]Book, error) {
 // The user input is not parameterized. Instead of using fmt.Sprintf() to build
 // the query, you should be using a parameterized query.
 func ReadQuery(r string) ([]Book, error) {
-	// Fix: rows, err := DB.Query("SELECT * FROM books WHERE read = ?", r)
-	rows, err := DB.Query(fmt.Sprintf("SELECT * FROM books WHERE read = '%s'", r))
+	rows, err := DB.Query("SELECT * FROM books WHERE read = ?", r)
+	// rows, err := DB.Query(fmt.Sprintf("SELECT * FROM books WHERE read = '%s'", r))
 	if err != nil {
 		return nil, err
 	}
